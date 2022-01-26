0b137b2d838a6fe38c0c2959b488d00295698ea9
Omar <omarsalem0721@gmail.com>
2022-01-27 00:27:47 +0200

    fetch the api and add from for post

---
+++ src/modules/api.js
@@ -0,0 +1,25 @@
+const id = '4XEvJenIyQnAlKwZCQr6';
+
+export const getScores = async () => {
+  const response = await fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`,
+  );
+  const { result } = await response.json();
+  return result;
+};
+
+export const setScores = async (data) => {
+  const response = await fetch(
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    },
+  );
+  const { result } = await response.json();
+
+  return result;
+};
