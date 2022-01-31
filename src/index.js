3c03705376c1db0a1606e3594acff6cd0f450ff7
Omar <omarsalem0721@gmail.com>
2022-01-31 10:01:28 +0200

    fetch the api and render data

--- src/index.js
+++ src/index.js
@@ -1,1 +1,24 @@
 import './style.css';
+import { setScores, getScores } from './modules/api';
+import renderScores from './modules/renderScores';
+
+window.onload = async () => {
+  renderScores(await getScores());
+};
+const refreshBtn = document.querySelector('.refresh button');
+
+refreshBtn.addEventListener('click', async () => {
+  const list = await getScores();
+  renderScores(list);
+});
+
+const form = document.querySelector('form');
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const newScore = {
+    user: document.querySelectorAll('input')[0].value.trim(),
+    score: document.querySelectorAll('input')[1].value.trim(),
+  };
+  form.reset();
+  await setScores(newScore);
+});
