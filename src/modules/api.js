4d3d5b62165c3c4fcbc2f2bfbaef6877797306fa
Omar <omarsalem0721@gmail.com>
2022-01-31 10:04:06 +0200

    Fix linters erroe

--- src/modules/api.js
+++ src/modules/api.js
@@ -2,7 +2,7 @@ const id = '4XEvJenIyQnAlKwZCQr6';
 
 export const getScores = async () => {
   const response = await fetch(
-    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`
+    `https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/${id}/scores`,
   );
   const { result } = await response.json();
   return result;
@@ -17,7 +17,7 @@ export const setScores = async (data) => {
         'Content-type': 'application/json',
       },
       body: JSON.stringify(data),
-    }
+    },
   );
   const { result } = await response.json();
 
